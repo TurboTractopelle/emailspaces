fe6de5423f0e0f587a63cc3e8763595f7ce6e040
Your Name <you@example.com>
2019-07-21 09:14:51 +0200

    feat: add fake journal data

--- src/controllers/journals.js
+++ src/controllers/journals.js
@@ -4,6 +4,13 @@ const journalsData = require("../../fixtures/journals.json")
 function setupJournals(server) {
   const router = server.router;
 
+  server.get("/", (req,res,next)=>{
+    res.send("API guide coming one day")
+    next()
+  })
+
+  server.get({name: "getJournal", path:"/journals/:id" }, getJournal)
+
   server.get(
     {
       name: "journals",
@@ -17,6 +24,23 @@ function setupJournals(server) {
 
 function journals(req, res, next) {
   res.send(journalsData);
+  next()
+}
+
+function getJournal(req, res, next){
+if(req.params.id && req.params.id >5){
+  res.status(404)
+  res.send("nope")
+} else {
+  res.send(journalsData.filter(journal => journal.id === req.params.id)[0])
+  next()
+
+}
+
+
+
+
+
 }
 
 module.exports = setupJournals;
